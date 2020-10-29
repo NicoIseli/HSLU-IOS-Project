4f318623e28f933d79cb0eace9c77154e1ae6b35
Nico Iseli <nico.iseli@stud.hslu.ch>
2020-10-29 16:54:37 +0100

    Add example code to fill tables and change views with row data

--- FoodPrint/FoodPrint/testFiles/testConfiguration.swift
+++ FoodPrint/FoodPrint/testFiles/testConfiguration.swift
@@ -7,19 +7,23 @@
 
 import Foundation
 
-struct languages {
-    let english: String = "english"
-    let german: String = "german"
+class Configuration {
+    
+    struct languages {
+        let english: String = "english"
+        let german: String = "german"
+    }
+
+    struct preferences {
+        let vegetarian: String = "vegetarian"
+        let vegan: String = "vegan"
+        let glutenfree: String = "glutenfree"
+        let vegetables: String = "vegetables"
+        let fruits: String = "fruits"
+        let noDiary: String = "noDiary"
+        let noNuts: String = "noNuts"
+        let meat: String = "meat"
+        let fish: String = "fish"
+    }
 }
 
-struct preferences {
-    let vegetarian: String = "vegetarian"
-    let vegan: String = "vegan"
-    let glutenfree: String = "glutenfree"
-    let vegetables: String = "vegetables"
-    let fruits: String = "fruits"
-    let noDiary: String = "noDiary"
-    let noNuts: String = "noNuts"
-    let meat: String = "meat"
-    let fish: String = "fish"
-}
--- FoodPrint/FoodPrint/testFiles/testHTTPCall.swift
+++ FoodPrint/FoodPrint/testFiles/testHTTPCall.swift
@@ -8,76 +8,149 @@
 import Foundation
 
 
-
-
-
-// MARK: - URL's
-let urlProductList = URL(string: "https://")!
-let urlLabels = URL(string: "https://")!
+    // MARK: - URL's
+    let urlProductList = URL(string: "https://")!
+    let urlLabels = URL(string: "https://")!
 
 
 
 
 
-// MARK: - Data Structures for fetching data
+    // MARK: - Data Structures for fetching data
 
-// Structs containing information about the products
-struct ProductList: Codable {
-    let products: [Product]
-}
+    // Structs containing information about the products
+    struct ProductList: Codable {
+        let products: [Product]
+    }
 
-struct Product: Codable {
-    let name: String
-    let description : String
-    let season: String
-    let lables: [String]
-}
+    struct Product: Codable {
+        let name: String
+        let description : String
+        let season: String
+        let lables: [String]
+    }
 
-// Struct containing information for the label
-struct Label: Codable{
-    let labels: [String]
-}
-
-
+    // Struct containing information for the label
+    struct Label: Codable{
+        let labels: [String]
+    }
 
 
-// MARK: - Functions to get data over HTTP
 
-// Function to get products
-func fetchProducts() -> [Product] {
-    
-    var products: [Product] = []
-    
-    do{
-        let encodedData = try Data(contentsOf: urlProductList)
+
+    // MARK: - Functions to get data over HTTP
+
+    // Function to get products
+    func fetchProducts() -> [Product] {
         
-        let decodedData = try JSONDecoder().decode(ProductList.self, from: encodedData)
+        //What about using a class instead of a struct? Calculate Rating etc. use it throughout several files etc.
         
-        products = decodedData.products
+        var products: [Product] = []
         
-    } catch {
-        print("error")
+        do{
+            let encodedData = try Data(contentsOf: urlProductList)
+            
+            let decodedData = try JSONDecoder().decode(ProductList.self, from: encodedData)
+            
+            products = decodedData.products
+            
+        } catch {
+            print("error")
+        }
+        
+        return products
+    }
+
+    // Function to get Labels
+    func fetchLabels() -> [String] {
+        
+        var products: [String] = []
+        
+        do{
+            let encodedData = try Data(contentsOf: urlLabels)
+            
+            let decodedData = try JSONDecoder().decode(Label.self, from: encodedData)
+            
+            products = decodedData.labels
+            
+        } catch {
+            print("error")
+        }
+        
+        return products
     }
     
-    return products
-}
+    
+    
+    
+    
+    // MARK: - Functions to fill Tables
+    
+    
+    /*
+        let products : [Product] = fetchProducts()
+        
+        func reloadProducts() {
+            self.tableView.reloadData()
+        }
+ 
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+            return products.count
+        }
+ 
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+            cell.textLabel?.text = "\(products[indexPath.row].name) \(products[indexPath.row].rating)"
+            return cell
+         }
+ 
+        override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return "Don't even know if this is necessary"
+         }
+ 
+ 
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+             
+            let indexPath = self.tableView.indexPathForSelectedRow!
+             
+            if segue.identifier == "product", let productViewController = segue.destination as? ProductViewController{
+                productViewController.product = products[indexPath.row]
+             }
+         }
+    */
 
-// Function to get Labels
-func fetchLabels() -> [String] {
-    
-    var products: [String] = []
-    
-    do{
-        let encodedData = try Data(contentsOf: urlLabels)
-        
-        let decodedData = try JSONDecoder().decode(Label.self, from: encodedData)
-        
-        products = decodedData.labels
-        
-    } catch {
-        print("error")
-    }
-    
-    return products
-}
 
+    //MARK: - Class-Code for ProductViewController
+    /*
+         import UIKit
+
+         class ProductViewController: UIViewController {
+
+            @IBOutlet weak var nameLabel: UILabel!
+             
+            @IBOutlet weak var informationLabel: UILabel!
+ 
+            @IBOutlet weak var informationLabel: UILabel!
+             
+            var product: Product
+             
+            override func viewDidLoad() {
+                     super.viewDidLoad()
+                 }
+             
+            override func viewWillAppear(_ animated: Bool) {
+                 super.viewWillAppear(animated)
+                 
+                 self.firstNameLabel.text = self.product!.firstName
+                 self.lastNameLabel.text = self.product!.lastName
+                 self.postalCodeLabel.text = String(self.product!.rating)
+             }
+         }
+ 
+ 
+    */
+
+    
